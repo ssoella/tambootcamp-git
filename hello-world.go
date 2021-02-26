1460267180bda8dd72232076f522b6018de5755a
Sven Soella <ssoella@vmware.com>
2021-02-26 18:39:25 +0100

    checkin lines 3/4

--- hello-world.go
+++ hello-world.go
@@ -1,6 +1,6 @@
-# edited first comment
+# add a new comment
 # add a second comment
-# add a third comment
+# local changes
 # add a fourth comment
 # file:  hello-world.go
 # description:  my very first go program
